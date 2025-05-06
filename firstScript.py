30336bc0f516bcbaeaf3485631d0e083c6a41c85
Zane Falcao <falcaozane@gmail.com>
2025-05-06 10:40:01 +0530

    selenium 😭 for arcon

---
+++ firstScript.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+# Specify the path to your msedgedriver.exe
+edge_driver_path = "F:\\edgedriver_win64\\msedgedriver.exe"
+
+#browser = webdriver.Chrome("C:\Drivers\network\chrome-win64.exe")
+#browser =  webdriver.Edge("F:\edgedriver_win64\msedgedriver.exe")
+
+# Create a Service object with the executable path
+service = Service(executable_path=edge_driver_path)
+
+# Initialize the Edge browser with the Service object
+browser = webdriver.Edge(service=service)
+url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+
+browser.get(url)
+print("Open Browser")
---
+++ secondscript.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+
+# Specify the path to your msedgedriver.exe
+edge_driver_path = "F:\\edgedriver_win64\\msedgedriver.exe"  # Replace with the actual path to your msedgedriver.exe
+
+# Create a Service object
+service = Service(executable_path=edge_driver_path)
+
+# Initialize the Edge browser with the Service object
+driver = webdriver.Edge(service=service)
+
+url = "https://practicetestautomation.com/practice-test-login/"
+driver.get(url)
+username_enter = "student"
+Password_Enter = "Password123"
+username = driver.find_element(By.ID, value="username")
+username.send_keys(username_enter)
+Password = driver.find_element(By.ID, value="password")
+Password.send_keys(Password_Enter)
+Submit = driver.find_element(By.ID, value="submit")
+Submit.click()
+Logout = driver.find_element(By.LINK_TEXT, value="Log out")
+Logout_text = Logout.text
+Expected_text = "Log out"
+assert Logout_text == Expected_text
+
+print("Session correct")
+
+# You might want to add a driver.quit() at the end to close the browser
+# driver.quit()
