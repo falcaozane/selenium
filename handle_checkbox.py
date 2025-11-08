c1a28ccdbd110896588c693d5a200cf2a20d9524
Falcao Zane Vijay <112317470+falcaozane@users.noreply.github.com>
2025-11-08 21:11:23 +0530

    ok transferring to office laptop

--- handle_checkbox.py
+++ handle_checkbox.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 
+
 # Specify the path to your msedgedriver.exe
 edge_driver_path = "E:\\QA\\selenium\\msedgedriver.exe"  # Replace with the actual path to your msedgedriver.exe
 
@@ -12,22 +13,13 @@ service = Service(executable_path=edge_driver_path)
 # Initialize the Edge browser with the Service object
 driver = webdriver.Edge(service=service)
 
-url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+url = "https://www.tutorialspoint.com/selenium/practice/check-box.php"
 driver.get(url)
 
-time.sleep(5)
+time.sleep(3)
 
 driver.fullscreen_window()
 
-title = driver.title
-print(title)
 
-username_enter = "Admin"
-Password_Enter = "admin123"
+time.sleep(10)
 
-username = driver.find_element(By.name, value="username")
-username.send_keys(username_enter)
-Password = driver.find_element(By.name, value="password")
-Password.send_keys(Password_Enter)
-Submit = driver.find_element(By.ID, value="submit")
-Submit.click()
