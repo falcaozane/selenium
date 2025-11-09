6af86c25a5392fa421f44a9468f7186673a16d7b
falcaozane <falcaozane@gmail.com>
2025-11-09 17:06:47 +0530

    form practice

--- dropdown.py
+++ dropdown.py
@@ -13,19 +13,20 @@ service = Service(executable_path=edge_driver_path)
 # Initialize the Edge browser with the Service object
 driver = webdriver.Edge(service=service)
 
-url = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php"
+url = "https://the-internet.herokuapp.com/dropdown"
 driver.get(url)
-
-time.sleep(3)
-
-driver.fullscreen_window()
-
-check_box = driver.find_element(By.ID, "hobbies")
-
-if not check_box.is_selected():
-    check_box.click()
-    time.sleep(3)
 
 
 time.sleep(5)
 
+dropdown_option_1 = driver.find_element(By.XPATH,"//select[@id='dropdown']/option[@value='1']")
+
+dropdown_option_2 = driver.find_element(By.XPATH,"//select[@id='dropdown']/option[@value='2']")
+
+dropdown_option_1.click()
+
+time.sleep(3)
+
+dropdown_option_2.click()
+
+time.sleep(5)
--- practice_form.py
+++ practice_form.py
@@ -16,9 +16,36 @@ driver = webdriver.Edge(service=service)
 url = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php"
 driver.get(url)
 
+driver.fullscreen_window()
+
+title = driver.title
+print(title)
+
+
+username_enter = "student"
+email_enter = "student@gmail.com"
+mobile_enter = "9028921961"
+dob_enter = "21-08-2003"
+
+name = driver.find_element(By.ID, value="name")
+name.send_keys(username_enter)
+email = driver.find_element(By.ID, value="email")
+email.send_keys(email_enter)
+
 time.sleep(3)
 
-driver.fullscreen_window()
+gender = driver.find_element(By.XPATH,value="//label[text()='Male']/preceding-sibling::input[@type='radio']")
+gender.click()
+time.sleep(3)
+
+mobile = driver.find_element(By.NAME,value="mobile")
+mobile.send_keys(mobile_enter)
+time.sleep(3)
+
+dob = driver.find_element(By.XPATH,value="//input[@type='date']")
+dob.send_keys(dob_enter)
+
+time.sleep(3)
 
 check_box = driver.find_element(By.ID, "hobbies")
 
@@ -26,6 +53,4 @@ if not check_box.is_selected():
     check_box.click()
     time.sleep(3)
 
-
-time.sleep(5)
-
+time.sleep(10)
